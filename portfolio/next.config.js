3b8309b0cd1de6a4d155155d468b50af65a21e49
krishv2 <krishv_11@hotmail.com>
2022-03-26 20:41:50 +0000

    adding this too

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/krishv2/krishv2.github.io/gh-pages/'
 }
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = nextConfig
