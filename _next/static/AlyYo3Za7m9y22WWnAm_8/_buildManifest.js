588122d3467071c2e7b448659d6524ce53eb41cb
krishv2 <61357583+krishv2@users.noreply.github.com>
2022-03-27 21:08:49 +0000

    Deploying to gh-pages from @ krishv2/krishv2.github.io@9331c51e7a2072fbac6b78efc5485f67cbc1dd9b 🚀

---
+++ _next/static/AlyYo3Za7m9y22WWnAm_8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/762-b9df1501bed6c9b5.js","static/chunks/pages/index-b17d75a3c5437b76.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
