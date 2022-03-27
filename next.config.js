9331c51e7a2072fbac6b78efc5485f67cbc1dd9b
krishv2 <krishv_11@hotmail.com>
2022-03-27 22:07:42 +0100

    adding image loader

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 
